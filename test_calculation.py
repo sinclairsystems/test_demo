947f6d1eac6e8b539cc6e03b29e9c04070266917
csinclair <chris@sinclairsystems.com>
2022-08-05 15:23:00 +1200

    first commit for demo

--- test_calculation.py
+++ test_calculation.py
@@ -5,7 +5,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_add(self):
         actual_result = calculation.add(5, 6)
-        expected_result = 11
+        expected_result = 13
         self.assertEqual(expected_result, actual_result)  # add assertion here
 
 
